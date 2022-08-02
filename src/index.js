c2ce9ce6d59d2ad5b86671ac2a0361f7b36a1f8f
Nancat74 <nastyav74@mail.ru>
2022-08-02 10:11:45 +0500

    feat: add code in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  while (true) {
+    const bracketsArray = bracketsConfig.map((g) => {
+      return g.join("");
+  });
+        let lengthStringUpdated = str.length;
+    for (let e = 0; e < bracketsArray.length; e++) {
+      str = str.replace(bracketsArray[e], "");
+  }
+    if (str.length === 0 || lengthStringUpdated === str.length) {
+      break;
+  }
+}
+  return str.length === 0;
 }
